b4e8894e00c71db60f4a585ac72ee2cb06c2a2c4
timotheemcdonald <timotheemcdonald@gmail.com>
2020-11-22 21:54:02 -0500

    board set up

--- app.js
+++ app.js
@@ -35,5 +35,32 @@ document.addEventListener('DOMContentLoaded', () => {
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     ]
 
+    //Legend
+    //0 - pac dot
+    //1 - wall
+    //2 - ghost lair
+    //3 - power pellet
+    //4 - empty
+
+    const squares = []
+
+    function createBoard(){
+        for (let i=0; i<layout.length; i++) {
+            const square = document.createElement('div')
+            grid.appendChild(square)
+            squares.push(square)
+
+            //add layout to the board
+            if(layout[i] === 0){
+                squares[i].classList.add('pac-dot')
+            } else if (layout[i] === 1) {
+                squares[i].classList.add('wall')
+            } else if (layout[i] === 3) {
+                squares[i].classList.add('power-pellet')
+            }
+        }
+    }
+
+    createBoard()
     
 })
